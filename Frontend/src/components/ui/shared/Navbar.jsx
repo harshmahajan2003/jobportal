41fba18eb62bcd0694331dc6c3a333806b7ec002
harshmahajan2003 <harshmahajan552@gmail.com>
2025-06-22 01:30:10 +0530

    frontend start

---
+++ Frontend/src/components/ui/shared/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Navbar = () => {
+    return (
+        <div className='bg-white'>
+            <h1 className='text-2xl font-bold '>Job<span className='text-[#F830002]'>Portal</span></h1>
+            
+        </div>
+    )
+}
+export default Navbar
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,17 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
-import { fileURLToPath } from 'url'
+import tailwindcss from '@tailwindcss/vite'
 
-// Fix __dirname in ES module
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  plugins: [react(), tailwindcss()],
 })
